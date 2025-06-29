ba244443543a8fe9389368540034065d4f60fae0
FALY18 <falitinazandrinolona@gmail.com>
2025-06-29 16:57:04 +0300

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Toaster } from 'sonner';
 export const metadata = {
 	title: "planteforme de vente",
 	description: "Multiplateforme de vente en ligne",
+	keywords: ["e-commerce", "vente en ligne", "produits", "multiplateforme"],
+  	robots: "index, follow",
 };
 
 export default function RootLayout({
